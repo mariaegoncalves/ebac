488f5e27165b8ce685b5b251bb9f9c30e744ce54
Maria Eduarda Gonçalves <mariaeduardag2605@gmail.com>
2024-10-21 22:22:26 -0300

    Adicionar funções de multiplicação e saudação em TypeScript

---
+++ typescript/dist/exercicio.js
@@ -0,0 +1,14 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudar(nome) {
+    return "Olá " + nome;
+}
+// Exemplos
+const resultadoMultiplicacao = multiplicar(5, 10);
+console.log(resultadoMultiplicacao);
+const mensagemSaudacao = saudar("Maria");
+console.log(mensagemSaudacao);
